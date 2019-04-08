8fcad01b71f70e750f0e34df0cb537430d4f6906
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-08 17:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function theBeatlesPlay (musicians, instruments) {
 function johnLennonFacts (facts) {
   var i = 0;
   while (i < facts.length) {
-    
+    return facts[i] + "!!!"
+    i++
   }
 }
