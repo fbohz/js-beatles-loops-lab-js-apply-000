3c7054a935cf4f99b999af9ac17e2bec6fbd6222
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-08 17:41:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (musicians, instruments) {
   var arr = []
   for (var i = 0; i < musicians.length; i++) {
     for (var b = 0; b < instruments.length; b++) {
-      arr = musicians[i] + " plays " + instruments[b]
+      arr.push (musicians[i] + " plays " + instruments[b])
      
     }
   }
