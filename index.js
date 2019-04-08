dd21ddc1b66ad7b6b7146d3403e4f0bb7e59d3a0
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-08 17:05:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function theBeatlesPlay (musicians, instruments) {
   var arr = []
   for (i = 0; i < musicians.length; i++) {
-  
+    for (b = 0; b < instruments.length; b++) {
+      
+    }
   }
 }
