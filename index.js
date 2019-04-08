3785479795c1af61246ae5d53b827a3712fe3ff6
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-08 17:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
   var arr = []
-  for (for i = 0; i < musicians.length; i++) {
+  for (i = 0; i < musicians.length; i++) {
   
   }
 }
